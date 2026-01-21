add990b715b6bc897e73861657c863aa0a18da71
churka <yalublupubg228@gmail.com>
2026-01-22 00:19:55 +1000

    created example a sidebar

--- static/js/chat/chatDetails.js
+++ static/js/chat/chatDetails.js
@@ -1,4 +1,10 @@
-let title_chat_item = document.querySelector('.chat-title');
-let title_chat_top_bar = document.querySelector('.top-bar-chat-title');
+const title_chat = document.querySelector('.title-chat');
+const title_chat_item = document.querySelector('.chat-title');
+const title_chat_top_bar = document.querySelector('.top-bar-chat-title');
+const title_chat_side_bar = document.querySelector('.side-bar-chat-title')
 
-title_chat_top_bar.textContent = title_chat_item.textContent
+title_chat_item.textContent = title_chat.textContent;
+title_chat_top_bar.textContent = title_chat.textContent;
+title_chat_side_bar.textContent = title_chat.textContent;
+
+const numbers = {}
